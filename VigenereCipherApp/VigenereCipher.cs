476a5260005bcb39330d8c1af636e71d5b23a3c8
Korzh <Korolmedvedey@gmail.com>
2026-04-28 14:03:58 +0300

    Пункт 4. Реализация шифра и автотесты

--- VigenereCipherApp/VigenereCipher.cs
+++ VigenereCipherApp/VigenereCipher.cs
@@ -1,12 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace VigenereCipherApp
 {
-    internal class VigenereCipher
+    public class VigenereCipher
     {
+        private const string EnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string RussianAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        public string Encrypt(string text, string key)
+        {
+            ValidateInputs(text, key);
+            return ProcessText(text, key, true);
+        }
+
+        public string Decrypt(string text, string key)
+        {
+            ValidateInputs(text, key);
+            return ProcessText(text, key, false);
+        }
+
+        private void ValidateInputs(string text, string key)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("Ключ не может быть пустым", nameof(key));
+        }
+
+        private string ProcessText(string text, string key, bool encrypt)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var result = new StringBuilder();
+            int keyIndex = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    string alphabet = GetAlphabet(c);
+                    bool isUpper = char.IsUpper(c);
+                    int shift = GetShift(key[keyIndex % key.Length], alphabet);
+                    int pos = GetPosition(c, alphabet);
+
+                    int newPos = encrypt
+                        ? (pos + shift) % alphabet.Length
+                        : (pos - shift + alphabet.Length) % alphabet.Length;
+
+                    char newChar = alphabet[newPos];
+                    result.Append(isUpper ? newChar : char.ToLower(newChar));
+                    keyIndex++;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+
+        private string GetAlphabet(char c)
+        {
+            char upper = char.ToUpper(c);
+            if (upper >= 'A' && upper <= 'Z')
+                return EnglishAlphabet;
+            return RussianAlphabet;
+        }
+
+        private int GetShift(char keyChar, string alphabet)
+        {
+            char upper = char.ToUpper(keyChar);
+            int index = alphabet.IndexOf(upper);
+            return index >= 0 ? index : 0;
+        }
+
+        private int GetPosition(char c, string alphabet)
+        {
+            return alphabet.IndexOf(char.ToUpper(c));
+        }
     }
 }
--- VigenereCipherTests/VigenereCipherTests.cs
+++ VigenereCipherTests/VigenereCipherTests.cs
@@ -1,14 +1,71 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VigenereCipherApp;
 
 namespace VigenereCipherTests
 {
     [TestClass]
     public class VigenereCipherTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private VigenereCipher _cipher;
+
+        [TestInitialize]
+        public void Setup()
         {
+            _cipher = new VigenereCipher();
+        }
+
+        
+        [TestMethod]
+        public void Encrypt_EnglishText_ReturnsCiphertext()
+        {
+            string result = _cipher.Encrypt("HELLO", "KEY");
+            Assert.AreEqual("RIJVS", result);  
+        }
+
+        [TestMethod]
+        public void Decrypt_EnglishCipher_ReturnsPlaintext()
+        {
+            string result = _cipher.Decrypt("RIJVS", "KEY"); 
+            Assert.AreEqual("HELLO", result);
+        }
+
+        [TestMethod]
+        public void EncryptDecrypt_Roundtrip_ReturnsOriginal()
+        {
+            string original = "Поддержка и тестирование";
+            string encrypted = _cipher.Encrypt(original, "ключ");
+            string decrypted = _cipher.Decrypt(encrypted, "ключ");
+            Assert.AreEqual(original, decrypted);
+        }
+
+        [TestMethod]
+        public void Encrypt_EmptyText_ReturnsEmpty()
+        {
+            string result = _cipher.Encrypt("", "key");
+            Assert.AreEqual("", result);
+        }
+
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Encrypt_NullText_ThrowsException()
+        {
+            _cipher.Encrypt(null, "key");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Encrypt_NullKey_ThrowsException()
+        {
+            _cipher.Encrypt("text", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Encrypt_EmptyKey_ThrowsException()
+        {
+            _cipher.Encrypt("text", "");
         }
     }
 }
