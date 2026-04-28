e6b151fd91a7e488f617c0d7465c5979f8e2e4cc
Korzh <Korolmedvedey@gmail.com>
2026-04-28 13:25:36 +0300

    Начало работы

---
+++ VigenereCipherTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VigenereCipherTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VigenereCipherTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("414281b7-f577-44f6-936c-18cd61ec6843")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VigenereCipherTests/VigenereCipherTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VigenereCipherTests
+{
+    [TestClass]
+    public class VigenereCipherTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
